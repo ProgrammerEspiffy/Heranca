ef34fe6f9dc86e785a5f9e092484ddf6c160f2b8
ProgrammerEspiffy <espiffotheraccoon@gmail.com>
2024-06-07 15:51:02 -0300

    Final Version

--- src/application/Program.java
+++ src/application/Program.java
@@ -46,5 +46,16 @@ public class Program {
         Account acc7 = new BusinessAccount(1006, "Tairone", 1000.0,500.0);
         acc7.withdraw(200.0);
         System.out.println(acc7.getBalance());
+
+        //Introdução ao Polimorfismo
+
+        Account x = new Account(1020, "Alex", 1000.0);
+        Account y = new SavingsAccount(1023, "Maria", 1000.0,0.01);
+
+        x.withdraw(50.0);
+        y.withdraw(50.0);
+
+        System.out.println(x.getBalance());
+        System.out.println(y.getBalance());
     }
 }
