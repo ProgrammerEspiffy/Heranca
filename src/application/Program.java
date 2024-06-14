c7d2f5b3c1f4dd0585416a5bd55ac493d02006c0
ProgrammerEspiffy <espiffotheraccoon@gmail.com>
2024-06-14 09:51:32 -0300

    Abstract class Update

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,58 +4,34 @@ import entities.Account;
 import entities.BusinessAccount;
 import entities.SavingsAccount;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class Program {
     public static void main(String[] args) {
 
-        Account acc = new Account(1001, "Alex", 1000.0);
-        BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+        Locale.setDefault(Locale.US);
+        List<Account> list = new ArrayList<>();
 
-        // UPCASTING
+        list.add(new SavingsAccount(1001, "Alex", 500.0, 0.01));
+        list.add(new BusinessAccount(1002, "Maria", 1000.0, 400.0));
+        list.add(new SavingsAccount(1004, "Bob", 300.0, 0.01));
+        list.add(new BusinessAccount(1005, "Anna", 500.0, 500.0));
 
-        Account acc1 = bacc;
-        Account acc2 = new BusinessAccount(1003,"Bob", 0.0,200.0);
-        Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
-
-        // DOWNCASTING
-
-
-        BusinessAccount acc4 = (BusinessAccount) acc2;
-        acc4.loan(100.0);
-
-        //BusinessAccount acc5 = (BusinessAccount)acc3;
-        if(acc3 instanceof BusinessAccount){
-            BusinessAccount acc5 = (BusinessAccount)acc3;
-            acc5.loan(200.0);
-            System.out.println("Loan!");
+        double sum = 0.0;
+        for (Account acc : list) {
+            sum+= acc.getBalance();
         }
 
-        if (acc3 instanceof SavingsAccount){
-            SavingsAccount acc5 = (SavingsAccount)acc3;
-            acc5.updateBalance();
-            System.out.println("Update!");
+        System.out.printf("Total balance: %.2f%n", sum);
+
+        for (Account acc : list) {
+            acc.deposit(10.0);
         }
 
-        //@Override
-        acc.withdraw(200.0);
-        System.out.println(acc.getBalance());
-
-        Account acc6 = new SavingsAccount(1005, "Joseph", 1000.0, 0.1);
-        acc6.withdraw(200.0);
-        System.out.println(acc6.getBalance());
-
-        Account acc7 = new BusinessAccount(1006, "Tairone", 1000.0,500.0);
-        acc7.withdraw(200.0);
-        System.out.println(acc7.getBalance());
-
-        //Introdução ao Polimorfismo
-
-        Account x = new Account(1020, "Alex", 1000.0);
-        Account y = new SavingsAccount(1023, "Maria", 1000.0,0.01);
-
-        x.withdraw(50.0);
-        y.withdraw(50.0);
-
-        System.out.println(x.getBalance());
-        System.out.println(y.getBalance());
+        for (Account acc : list) {
+            System.out.printf("Updated balance for %d: %.2f%n", acc.getNumber(), acc.getBalance());
+        }
     }
 }
